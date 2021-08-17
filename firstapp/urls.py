2c535b1c4be443391f5284451fc3e1d074b27548
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-08-17 21:10:02 +0900

    httpResponseの基礎を学習。urlから値を取得できた。

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = "firstapp"
 
 urlpatterns = [
-    path("hello", views.index, name="index")
+    path("hello", views.index, name="index"),
+    path("page/<str:user_name>", views.user_page, name="user_page"),
+    path("page_num/<int:number>/<str:user_name>", views.number_page, name="number_page")
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>Hello, World!</h1>")
+
+
+def user_page(request, user_name):
+    return HttpResponse(f'<h1>{user_name}\'s page</h1>')
+
+
+def number_page(request, number, user_name):  # httpResponseに渡すものは複数でも可能
+    user_name = user_name.upper()
+    return HttpResponse(f'<h1>{number}番の{user_name}のページですよ〜</h1>')
