bb3a30843ae3f7b4258e2cc5440268fc176181fd
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-08-21 11:50:53 +0900

    migrateコマンドについて学習。そういうことだったのか〜。

--- firstapp/models.py
+++ firstapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Language(models.Model):
+    fav = models.CharField(max_length=50)
+    like = models.CharField(max_length=40)
+
+
+class Framework(models.Model):
+    like1 = models.CharField(max_length=40)
+    like2 = models.CharField(max_length=40)
