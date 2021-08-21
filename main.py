13b67510ef7c868604fbfbcc0c58641b704226fb
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-08-21 21:46:41 +0900

    DBのデータを更新する方法を学習。

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "first_project.settings")
 
 from django import setup
+from django.utils import timezone
+import pytz
 
 setup()
 
@@ -27,19 +29,42 @@ from firstapp.models import Language
 # print(created)  # booleanで成功したかどうかが表示
 
 # 全てのデータを取得する
-languages = Language.objects.all()
-for language in languages:
-    print(language.id, language.startday, language)
+# languages = Language.objects.all()
+# for language in languages:
+#     print(language.id, language.startday, language)
+#
+# # 特定のデータのみ取得する (以下の結果は全て同じ)
+# language = Language.objects.get(id=1)
+# print(language)
+# languages = Language.objects.get(pk=1)
+# print(language)
+# language = Language.objects.get(fav="react")
+# print(language)
+#
+# # filterでデータを絞りこんで取得する
+# languages = Language.objects.filter(fav="react").all()
+# for language in languages:
+#     print(language)
 
-# 特定のデータのみ取得する (以下の結果は全て同じ)
-language = Language.objects.get(id=1)
-print(language)
-languages = Language.objects.get(pk=1)
-print(language)
-language = Language.objects.get(fav="react")
-print(language)
+# 更新処理
+# language = Language.objects.get(id=1)
+# print(language)
+# language.like = "best"
+# language.updated_at = timezone.datetime.now(pytz.timezone("Asia/Tokyo"))
+# language.save()
 
-# filterでデータを絞りこんで取得する
-languages = Language.objects.filter(fav="react").all()
-for language in languages:
-    print(language)
+
+# 複数の更新処理をする場合
+languages = Language.objects.filter(memo__isnull=True).all()
+# print(languages)
+# 下の処理だと一件毎にsaveをするため効率が悪い
+# for language in languages:
+#     language.memo = "updated!"
+#     language.updated_at = timezone.datetime.now(pytz.timezone("Asia/Tokyo"))
+#     language.save()
+
+# まとめてするなら下の方が効率的
+Language.objects.filter(memo__isnull=False).update(
+    memo="update2!",
+    updated_at=timezone.datetime.now(pytz.timezone("Asia/Tokyo"))
+)
